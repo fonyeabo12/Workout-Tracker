20fdbb883c33ce9524db0f01d7acf0f52fdbcaa0
FrankBlack <fonyeabo12@gmail.com>
2020-08-13 04:49:31 -0500

    Readme Updated

--- server.js
+++ server.js
@@ -12,11 +12,17 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+// mongoose.connect(MONGODB_URI,{  
+//     useNewUrlParser:true,
+//     useFindAndModify:false
+// })
+
+let Uri = "mongodb://localhost/workout";
+if (process.env.NODE_ENV == "production") {
+    uri = process.env.MONGODB_URI
+}
+mongoose.connect(Uri, {useNewUrlParser:true, useFindAndModify: false, useUnifiedTopology: true});
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
